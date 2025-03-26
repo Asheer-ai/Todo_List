efbaa7117316c4f77541f360c62e165e3242748d
Asheer-ai <asheerazam12@gmail.com>
2025-03-26 23:49:55 +0530

    Added Redux functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 
-import { useContext, useState } from 'react'
+import { useContext, useReducer, useState } from 'react'
 import './App.css'
 import AddTodo from './components/AddTodo/AddTodo'
 import TodoList from './components/TodoList/TodoList'
 import Todocontext from './Context/Todocontext'
+import todoReducer from './reducers/todoReducers'
 
 function App() {
-const [list,setList]=useState([
-  {id:1,todoData: 'todo 1',finished:false},
-  {id:2,todoData: 'todo 2',finished:true}
- ])
+// const [list,setList]=useState([
+//   {id:1,todoData: 'todo 1',finished:false},
+//   {id:2,todoData: 'todo 2',finished:true}
+//  ])
+
+const [list,dispatch]=useReducer(todoReducer,[]);
 
   return (
     <div id='conatiner'>
-    <Todocontext.Provider value={{list,setList}}>
-      <AddTodo updatelist={(todo)=>setList([...list,{id:list.length+1,todoData:todo,finished:false}])}/>
+    <Todocontext.Provider value={{list}}>
+      <AddTodo updatelist={(todo)=>{
+        dispatch({type:'add_todo',payload:{todoText:todo}})
+      }}/>
       <TodoList />
       </Todocontext.Provider>
     </div>
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -6,6 +6,31 @@ import './Todo.css';
 function TodoList() {
     const { list, setList } = useContext(Todocontext);
 
+    function onFinished(todo,isFinished){
+        const updatedList = list.map(t => {
+            if (t.id === todo.id) {
+                return { ...t, finished: isFinished };
+            }
+            return t;
+        });
+        setList(updatedList);
+    }
+    
+    function onDelete(todo){
+        const updatedList = list.filter(t => t.id !== todo.id);
+        setList(updatedList);
+    }
+
+    function onEdit(todo,todoText){
+        const updatedList = list.map(t => {
+            if (t.id === todo.id) {
+                return { ...t, todoData: todoText };
+            }
+            return t;
+        });
+        setList(updatedList);
+    }
+
     return (
         <div className="todo-list-container">
             <h2 className="todo-list-title">Todo List</h2>
@@ -16,28 +41,9 @@ function TodoList() {
                         id={todo.id} 
                         isFinished={todo.finished} 
                         todoData={todo.todoData} 
-                        changeFinished={(isFinished) => {
-                            const updatedList = list.map(t => {
-                                if (t.id === todo.id) {
-                                    return { ...t, finished: isFinished };
-                                }
-                                return t;
-                            });
-                            setList(updatedList);
-                        }}
-                        onDelete={() => {
-                            const updatedList = list.filter(t => t.id !== todo.id);
-                            setList(updatedList);
-                        }}
-                        onEdit={(todoText) => {
-                            const updatedList = list.map(t => {
-                                if (t.id === todo.id) {
-                                    return { ...t, todoData: todoText };
-                                }
-                                return t;
-                            });
-                            setList(updatedList);
-                        }}
+                        changeFinished={(isFinished)=>onFinished(todo,isFinished)}
+                        onDelete={() => onDelete(todo) }
+                        onEdit={(todoText) => onEdit(todo, todoText)}
                     />
                 ))}
             </div>
